7785f911abcb151cf81aae600eb22b745a16aad6
Chucklemunch <charlie.kotula@gmail.com>
2025-07-09 16:00:41 -0500

    Made NoteCoords to be able to manually specify location of notes on fretboard

--- muse-app/src/GuitarFretboard.tsx
+++ muse-app/src/GuitarFretboard.tsx
@@ -1,12 +1,36 @@
+import { noteCoords } from '../utilities/NoteCoords'
+
+
 export default function GuitarFretboard() {
 
 
   return (
-    <>
-        <img src='public/prs-neck.png' style={{
-            width: '75%',
-            transform: 'rotate(225deg)'
-        }}/>
-    </>
+    <div style={{ position: 'relative', width: '100%' }}>
+      <img src='public/prs-neck.png' style={{
+          width: '100%',
+        }}
+      />
+      {Object.entries(noteCoords).map(([stringName, frets]) =>
+        Object.entries(frets).map(([fretNum, { x, y }]) => (
+          <button
+            key={`${stringName}-${fretNum}`}
+            style={{
+              position: 'absolute',
+              left: `${x}%`,
+              top: `${y}%`,
+              width: '12px',
+              height: '12px',
+              borderRadius: '50%',
+              backgroundColor: 'white',
+              border: '1px solid #222',
+              fontSize: '10px',
+              color: 'white',
+              cursor: 'pointer',
+            }}
+            title={`${stringName} fret ${fretNum}`}
+          />
+        ))
+      )}
+    </div>
   )
 }
